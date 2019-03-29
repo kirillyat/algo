3ac9b469bd38c7e43acc935a7976083778b4d4c2
Kirill <kirillyat@gmail.com>
2019-03-29 22:33:26 +0300

    Update cnk.py

--- cnk.py
+++ cnk.py
@@ -7,5 +7,16 @@ def fact(k):
 def cnk(n, k):
     return(fact(n)/(fact(n-k)*fact(k)))
 
-for i in range(21):
-    print(cnk(20, i)
+def isPrime(a):
+    return not ( a < 2 or any(a % i == 0 for i in range(2, int(a ** 0.5) + 1)))
+
+def phi(n):
+    y = n
+    for i in range(2,n+1):
+        if isPrime(i) and n % i == 0:
+            y *= 1 - 1.0/i
+    return int(y)
+
+k, n = map(int, input().split())
+print(int(phi(int(fact(n)/fa
+                  
